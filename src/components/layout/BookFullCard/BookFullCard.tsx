fcbbef5280bc162ee4e76828454e572b8dcc8c25
shaulov <shineginn@gmail.com>
2023-10-28 17:48:59 +0300

    feat(bookfullcard): add back button and change styles

--- src/components/layout/BookFullCard/BookFullCard.tsx
+++ src/components/layout/BookFullCard/BookFullCard.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "@tanstack/router";
+import { useParams, Link } from "@tanstack/router";
 import type { FunctionComponent } from "../../../common/types";
 import { useAppSelector } from "../../../hooks/use-app-selector";
 import { getBooks } from "../../../store/books-slice/selectors";
@@ -9,13 +9,24 @@ function BookFullCard(): FunctionComponent {
   const currentBook = books.find(item => item.id === bookId);
 
   return (
-    <section className="flex flex-col md:flex-row flex-wrap gap-2 max-w-[1200px] m-auto px-10 pt-4 pb-12">
-      <h1 className="basis-full order-first">{currentBook?.fullTitle}</h1>
-      <p className="shrink">{currentBook?.subtitle}</p>
-      <p className="shrink">{currentBook?.fullAuthors}</p>
-      <p className="shrink">Page count: {currentBook?.pageCount}</p>
-      <p className="shrink">{currentBook?.description}</p>
-      <img className="order-first h-max" src={currentBook?.thumbnail} width={300} height={400} alt="Book cover" />
+    <section className="grid grid-cols-2 gap-4 max-w-[1200px] m-auto px-10 pt-4 pb-12">
+      <Link className="justify-self-start p-1 underline" to="/" activeOptions={{ exact: true }}>
+        &#8592; Go back
+      </Link>
+      <h1 className="col-span-2 mb-2 text-3xl">{currentBook?.fullTitle}</h1>
+      <div className="col-span-2 md:col-span-1 flex justify-center items-center p-2 bg-sky-50 rounded">
+        <img className="w-auto h-5/6 shadow-2xl" src={currentBook?.thumbnail} width={300} height={400} alt="Book cover" />
+      </div>
+      <dl className="col-span-2 md:col-span-1 flex flex-col justify-start gap-2 p-2 bg-sky-50 rounded">
+        <dt className="font-bold">Subtitle:</dt>
+        <dd>{currentBook?.subtitle}</dd>
+        <dt className="mt-2 font-bold">Authors:</dt>
+        <dd>{currentBook?.authors}</dd>
+        <dt className="mt-2 font-bold">Page count:</dt>
+        <dd>{currentBook?.pageCount} pages</dd>
+        <dt className="mt-2 font-bold">Description:</dt>
+        <dd className="grow p-2 bg-blue-100 rounded">{currentBook?.description}</dd>
+      </dl>
     </section>
   );
 }
